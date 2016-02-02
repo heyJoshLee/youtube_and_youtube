183a35467ab980a5030fa9ae92a6353f5be91eb4
jleewebdev <lee45276@gmail.com>
2016-02-02 10:38:07 +0800

    able and diable inputs

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,26 +1,41 @@
 $(function(){
 
-  $(".full-width iframe").attr("width", "100%");
-  $("iframe.half-width").attr("width", "50%");
-
-  $(".half-height").attr("height", "50%");
-
   $(window).on("resize", function(){
+    resize(); 
+  })
+
+  function resize() {
     var vph = $(window).height();
     var vpw = $(window).width();
-
-    console.log("height" + vph);
-    console.log("width" + vpw);
-
     $(".full-width iframe").attr("width", vpw + "px");
     $(".half-width iframe").attr("width", (vpw / 2) + "px");
     $(".full-height iframe").attr("height", vph + "px");
     $(".half-height iframe").attr("height", (vph / 2) + "px");
-
     $(".left + .left").css("top", vph/2 + "px");
     $(".right + .right").css("top", vph/2 + "px");
+  }
 
-  })
+  function twoInputs() {
+    $("#video-3-input").attr("disabled", "disabled");
+    $("#video-4-input").attr("disabled", "disabled");
+  }
+
+  function threeInputs(){
+    $("#videos-container").html(three_videos());
+    $("#video-3-input").removeAttr("disabled");
+    $("#video-4-input").attr("disabled", "disabled");
+  }
+
+  function fourInputs(){
+    $("#video-3-input").removeAttr("disabled");
+    $("#video-4-input").removeAttr("disabled");
+  }
+
+  function setupPage() {
+    $("#videos-container").html(two_videos());
+    resize();
+    twoInputs();
+  }
 
   $("#menu-launcher").on("click", function() {
     $("#menu").fadeToggle();
@@ -34,21 +49,25 @@ $(function(){
   var two_videos = Handlebars.compile($("#two-videos").html());
   $("#two-videos-layout").on("click", function() {
     $("#videos-container").html(two_videos());
+    twoInputs();
+    resize();
   })
 
   var three_videos = Handlebars.compile($("#three-videos").html());
   $("#three-videos-layout").on("click", function() {
-    $("#videos-container").html(three_videos());
+    threeInputs();
+    resize();
   })
 
   var four_videos = Handlebars.compile($("#four-videos").html());
   $("#four-videos-layout").on("click", function() {
     $("#videos-container").html(four_videos());
+    fourInputs();
+    resize();
   })
 
+  
 
-
-  //Handlebars.compile($("[script*=x-handlebars-template"]).html());
-  //Handlebars.registerPartial("name", $([script*=x-handlebars-partial"]).html());
-  // $("div").html(template_function({key: value}));
+  setupPage();
+  
 });
